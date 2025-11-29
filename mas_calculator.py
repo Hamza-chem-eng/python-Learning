fbbf2bd81f1dd64fe0da1a434f0cb6ca2f4d9834
Hamza-chem-eng <abwqwydrhmzt15@gmail.com>
2025-11-29 15:09:55 +0300

    Update mas_calculator.py

--- mas_calculator.py
+++ mas_calculator.py
@@ -3,9 +3,14 @@ Molar_mass = {
     "cl2":35.45,
     "ba(oh)":137.33
 }
-e = input("Choose the element (H2O,CL2,BA(OH) : ").lower()
-mas = float(input("enter the mas in (g) : "))
-if e in Molar_mass:
-    print("The mol you is ",mas*Molar_mass[e])
-else:
-    print("The value is not defined")
+while True:
+    e = input("Choose the element (H2O,CL2,BA(OH) : ").lower()
+    if e == "q":
+        print("goodbye")
+        break
+    mas = float(input("enter the mas in (g) : "))
+    if e in Molar_mass:
+        print("The number of mol is ", mas / Molar_mass[e])
+    else:
+        print("The value is not defined")
+
